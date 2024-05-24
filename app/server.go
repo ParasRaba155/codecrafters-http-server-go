0df8184c986d9fdd0314e49baf45f6e681d72e34
Paras Raba <parasgadhvi12345@gmail.com>
2024-05-24 13:46:05 +0530

    Add Stage 3 code for validating the request

--- app/server.go
+++ app/server.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"net"
+	"net/http"
 	"os"
-	"slices"
+	"strings"
 )
 
 var (
@@ -25,14 +28,40 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	_, err = conn.Write(createStatusOkResponse())
+	defer func() {
+		err := conn.Close()
+		if err != nil {
+			fmt.Println("Could not close the connection: ", err.Error())
+			os.Exit(1)
+		}
+	}()
+	var buf bytes.Buffer
+	_, err = io.Copy(&buf, conn)
 	if err != nil {
-		fmt.Println("Error Writing Status OK: ", err.Error())
+		fmt.Println("Could not copy from the conn: ", err.Error())
 		os.Exit(1)
 	}
+	requestBody := buf.Bytes()
+	parts := bytes.Split(requestBody, crlfBytes)
+	if isSlashRequest(parts) {
+		conn.Write(createResponse(200))
+	}
+	conn.Write(createResponse(404))
 }
 
-func createStatusOkResponse() []byte {
-	statusBytes := []byte(`200 OK`)
-	return slices.Concat(protocolBytes, spaceBytes, statusBytes, crlfBytes, crlfBytes)
+func createResponse(status int) []byte {
+	var b strings.Builder
+	b.Write(protocolBytes)
+	b.Write(spaceBytes)
+	b.WriteString(fmt.Sprintf("%d", status))
+	b.WriteString(http.StatusText(status))
+	b.WriteString(string(crlfBytes))
+	b.WriteString(string(crlfBytes))
+	return []byte(b.String())
+}
+
+func isSlashRequest(req [][]byte) bool {
+	reqLine := req[0]
+	reqLineSplits := bytes.Split(reqLine, spaceBytes)
+	return bytes.Equal(reqLineSplits[0], []byte("/"))
 }
